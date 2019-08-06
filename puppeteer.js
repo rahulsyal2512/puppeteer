cffa61441b8e6dde902e5b85485cb0ccb44dbb22
Rahul Syal <syal25121997@gmail.com>
2019-08-06 11:06:03 +0530

    added selector

--- puppeteer.js
+++ puppeteer.js
@@ -1,10 +1,22 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'screenshot.png'});
+  // await page.goto("https://example.com");
+  await page.goto("https://marketingplatform.google.com/about/");
+  // await page.screenshot({ path: "screenshot.png" });
+  // await page.pdf({ path: "hn.pdf", format: "A4" });
 
+  // const h1 = await page.evaluate(
+  //   () => document.querySelector("h1").textContent
+  // );
+  const h1 = await page.evaluate(() =>
+    Array.from(document.querySelector("div.compact h3.title")).map(partner => {
+      partner.innerText.trim();
+    })
+  );
+
+  console.log(h1);
   await browser.close();
 })();
